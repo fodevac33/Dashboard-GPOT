f821c2db9b14535d6fb6095add5cd748e5a4b797
Juan Daniel Gonzalez <juandanielgonzalez004@gmail.com>
2023-03-21 09:29:06 -0500

    Actualización Grafica
    Se añadida socket.io para la actualización automatica de la grafica

--- src/connection.js
+++ src/connection.js
@@ -1,41 +1,61 @@
 const { MongoClient } = require("mongodb");
-const express = require('express');
+const express = require("express");
 const app = express();
-const ejs = require('ejs');
+const ejs = require("ejs");
+const http =  require('http').createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(http);
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.get('/', async (req, res) => {
-  const uri = 'mongodb://admin:admin@54.164.104.223:27017/database';
-  const client = new MongoClient(uri);
-
-  try {
-    await client.connect();
-    let count = 1;
-    await simulationvoltage(client,count);
-    const [voltage,time] = await listByName(client);
-    res.render('grahps.ejs', { arrayVoltage: voltage, arrayTime : time }); 
-    await deleteVoltage(client,time,voltage)
-  } catch (error) {
-    console.error(error);
-  } finally {
-    await client.close();
-  }
+app.get('/', (req, res) => {
+  res.render('grahps.ejs');
 });
 
-app.listen(5500, () => console.log('Server running on port 5500'));
+
+io.on('connection', async (socket) => {
+  setInterval(async () => {
+  const uri = 'mongodb://admin:admin@54.85.148.227:27017/database';
+  const client = new MongoClient(uri);
+
+    try {
+      await client.connect();
+      const [voltage,time] = await listByName(client);
+      await deleteVoltage(client,time,voltage);
+      await simulationVoltage(client);
+      socket.emit('data', { arrayVoltage: voltage, arrayTime : time });
+      await client.close();
+    } catch (error) {
+      console.error(error);
+    } finally {
+      await client.close();
+    }
+  },2000);
+
+
+});
+
+http.listen(5500, () => console.log("Server running on port 5500"));
+
+
 
 async function listByName(client) {
-  const result = await client.db("database").collection("voltage").find({}, { projection: { time: 1 } });
+  const result = await client
+    .db("database")
+    .collection("voltage")
+    .find({}, { projection: { time: 1 } });
   const time = await result.toArray();
-  const result2 = await client.db("database").collection("voltage").find({}, { projection: { valor: 1 } });
+  const result2 = await client
+    .db("database")
+    .collection("voltage")
+    .find({}, { projection: { valor: 1 } });
   const voltage = await result2.toArray();
   const arrayTime = [];
   const arrayVoltage = [];
-  time.forEach(doc => {
+  time.forEach((doc) => {
     arrayTime.push(doc.time);
   });
-  voltage.forEach(doc => {
+  voltage.forEach((doc) => {
     arrayVoltage.push(doc.valor);
   });
 
@@ -50,18 +70,18 @@ async function listByName(client) {
 }
 
 
-async function simulationvoltage(client,count){
+async function simulationVoltage(client) {
+  const rand1 = Math.floor(Math.random() * 100);
   const rand2 = Math.floor(Math.random() * 100);
   const result = await client.db("database").collection("voltage").insertOne({
-    time: count++, 
-    valor: rand2
+    time: rand1,
+    valor: rand2,
   });
 }
 
-
-async function deleteVoltage(client, arrayTime, arrayVoltage){
+async function deleteVoltage(client, arrayTime, arrayVoltage) {
   const result = await client.db("database").collection("voltage").deleteMany({
     time: arrayTime[0],
-    valor: arrayVoltage[0]
+    valor: arrayVoltage[0],
   });
 }
