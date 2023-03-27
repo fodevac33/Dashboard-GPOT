db17929f3b43f8e0cabba274b93bd803b1685e8c
fodevac <fodevac@gmail.com>
2023-03-27 11:13:23 -0500

    Se movio archivos relevantes al Iot a aws

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,19 @@
 import express from "express";
+import { createServer } from "http";
+import { MongoClient } from "mongodb";
 import { Server } from "socket.io";
 const app = express();
+//Setup de socket.io
+const server = createServer(app);
 const io = new Server(server);
-io.listen(3000);
+//Conexion a la base de datos en el ec2
+const ec2_ip = "50.16.94.93";
+const uri = `mongodb://admin:admin@${ec2_ip}:27017/database`;
+const client = new MongoClient(uri);
+//Configuracion de la app en express
+app.set("view engine", "ejs");
 app.get("/", (req, res) => {
-    res.send("Hi from TypeScript");
+    res.render("grahps.ejs");
 });
+server.listen(5500, () => console.log("Server running on port 5500"));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,26 @@
 import express, {Express, Request, Response} from "express";
 import ejs from "ejs";
-import http from "http";
+import {createServer} from "http";
 import {MongoClient} from "mongodb";
 import {Server} from "socket.io"
 
 
-const app: Express = express()
+const app: Express = express();
 
+//Setup de socket.io
+const server = createServer(app);
 const io = new Server(server);
 
-io.listen(3000);
+//Conexion a la base de datos en el ec2
+const ec2_ip = "50.16.94.93";
+const uri = `mongodb://admin:admin@${ec2_ip}:27017/database`;
+const client = new MongoClient(uri);
 
+//Configuracion de la app en express
+app.set("view engine", "ejs");
 
 app.get("/", (req: Request, res: Response) => {
-    res.send("Hi from TypeScript");
+    res.render("grahps.ejs");
 });
+
+server.listen(5500, () => console.log("Server running on port 5500"));
