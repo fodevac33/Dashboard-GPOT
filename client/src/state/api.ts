a5cbb6b1fc5d282216c58ac412778fc955312740
Juan Daniel Gonzalez <juandanielgonzalez004@gmail.com>
2023-04-26 15:01:45 -0500

    git

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,15 +1,11 @@
 import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
-import { GetAQVoltageResponse, GetKpisResponse} from "./types";
+import { GetAQVoltageResponse} from "./types";
 
 export const api = createApi({
     baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),
     reducerPath: "main",
-    tagTypes: ["Kpis", "AQVoltages"],
+    tagTypes: ["AQVoltages"],
     endpoints: (build) => ({
-        getKpis: build.query<Array<GetKpisResponse>,void>({
-            query: ()=>"kpi/kpis/",
-            providesTags: ["Kpis"],
-        }),
         getAQVoltages: build.query<Array<GetAQVoltageResponse>,void>({
             query: ()=> "aqvoltage/aqvoltages",
             providesTags: ["AQVoltages"],
@@ -18,4 +14,4 @@ export const api = createApi({
 });
 
 
-export const {useGetKpisQuery, useGetAQVoltagesQuery} = api;
+export const {useGetAQVoltagesQuery} = api;
